8b87d967fc0642a91dca1301a0d8da93b4687587
Lin Dong <ldong@gatech.edu>
2015-02-23 21:05:28 -0800

    Fix image lag

--- js/script.js
+++ js/script.js
@@ -33,5 +33,18 @@ require(['jQuery', 'bootstrap','jQeasing', 'jQslimscroll','jQfullPage'], functio
       $('ul.nav > li').removeClass('active');
       $(this).addClass('active');
     });
+
+    // set images
+    var myPinterestSrc="https://raw.githubusercontent.com/ldong/myPinterest/iPhone/iPhoneDemo.gif";
+    $("#myPinterest").attr("src", myPinterestSrc);
+
+    var squid = "https://raw.githubusercontent.com/ldong/Squid/master/demo/demo.gif";
+    $('#iOSsquid').attr('src', squid);
+
+    var ldMineSweeper ="https://raw.githubusercontent.com/ldong/LDMineSweeper_iOS/master/demo/demo.gif"
+    $('#ldMineSweeper').attr('src',ldMineSweeper);
+
+    var ticTacToe= "https://raw.githubusercontent.com/ldong/tic_tac_toe/master/demo.png";
+    $('#ticTacToe').attr('src',ticTacToe);
   });
 });
